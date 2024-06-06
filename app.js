48cb5e009040d9d12a495aaf0aaca616021b0b5c
jitendra dasari <jitendra@quantsystemsinc.local>
2024-06-06 16:56:39 +0530

    new added changes made

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Learning to the point of getting handy at what we do'));
+app.get('/', (req, res) => res.send('Learning to the point of getting handy at what we do move ahead '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
